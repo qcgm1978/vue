772dd85cd7a225cf69f73f75255b56dab7c5c770
qcgm197874 <qcgm197874@gmail.com>
2016-12-28 15:37:47 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <hello></hello>
+        <bootstrap></bootstrap>
+    </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Bootstrap from './components/Bootstrap'
+export default {
+  name: 'app',
+  components: {
+    Hello,Bootstrap
+  }
+}
+</script>
+<style src="./assets/styles/global.scss" lang="scss"></style>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background:yellowgreen;
+}
+
+
+
+
+
+
+
+</style>
+<style lang="scss">
+    /* write SASS! */
+    html {
+        body {
+            #app {
+                background: yellow;
+            }
+        }
+    }
+</style>
---
+++ src/components/Bootstrap.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="bootstrap">
+        <span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'bootstrap',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+
+</style>
